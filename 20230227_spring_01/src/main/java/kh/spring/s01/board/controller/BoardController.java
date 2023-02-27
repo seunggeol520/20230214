bc773d9adb493dafa64113b5248b3dc589172381
SEUNGGEOL <seunggeol520@gmail.com>
2023-02-27 15:16:00 +0900

    spring(error)

--- 20230227_spring_01/src/main/java/kh/spring/s01/board/controller/BoardController.java
+++ 20230227_spring_01/src/main/java/kh/spring/s01/board/controller/BoardController.java
@@ -3,18 +3,29 @@ package kh.spring.s01.board.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import kh.spring.s01.board.model.service.BoardService;
 import kh.spring.s01.board.model.vo.BoardVo;
 
 @Controller
-@RequestMapping("board")
+@RequestMapping("/board")
 public class BoardController {
+	@Autowired
+	private BoardService service;
+	
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public void viewListBoard(Model model) {
+		model.addAttribute("boardList", service.selectList());
+	}
+	
 	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public void viewUpdateBoard() {
 		
